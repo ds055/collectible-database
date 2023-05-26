86acbbf2375203b81f4d9a96b29fef26baefd168
Trevor McGill <trevor.mcgill@icloud.com>
2023-05-25 19:51:50 -0500

    added route for action figures and files for cardroutes, coinroutes, and musicroutes

---
+++ controllers/api/actionFigureRoutes.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+const { ActionFigure } = require('../../models');
+
+// get all action figures
+router.get('/', async (req, res) => {
+  try {
+    const actionFigureData = await ActionFigure.findAll();
+    res.status(200).json(actionFigureData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get one action figure by id
+router.get('/:id', async (req, res) => {
+  try {
+    const actionFigureData = await ActionFigure.findByPk(req.params.id);
+    if (!actionFigureData) {
+      res.status(404).json({ message: 'No action figure found with that id!' });
+      return;
+    }
+    res.status(200).json(actionFigureData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// create a new action figure
+router.post('/', async (req, res) => {
+  try {
+    const actionFigureData = await ActionFigure.create(req.body);
+    res.status(200).json(actionFigureData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// update an action figure by id
+router.put('/:id', async (req, res) => {
+  try {
+    const actionFigureData = await ActionFigure.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!actionFigureData) {
+      res.status(404).json({ message: 'No action figure found with that id!' });
+      return;
+    }
+    res.status(200).json(actionFigureData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete an action figure by id
+router.delete('/:id', async (req, res) => {
+  try {
+    const actionFigureData = await ActionFigure.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!actionFigureData) {
+      res.status(404).json({ message: 'No action figure found with that id!' });
+      return;
+    }
+    res.status(200).json(actionFigureData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
