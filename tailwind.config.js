8e97696a909393a8291ef2ba99258a35850082dc
ShimmyShong <lattabun@gmail.com>
2023-05-25 20:47:02 -0400

    tailwind link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,handlebars}"],
+  content: ["./views/**/*.handlebars"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
