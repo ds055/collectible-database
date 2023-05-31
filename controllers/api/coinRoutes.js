a8d8c1a3ad3296695046175710c71059dbc8bc38
Trevor McGill <trevor.mcgill@icloud.com>
2023-05-31 18:44:24 -0500

    added code for coinRoutes.js

---
+++ controllers/api/coinRoutes.js
@@ -0,0 +1,79 @@
+const router = require('express').Router();
+const { Coin } = require('../../models');
+
+// get all Coins
+router.get('/', async (req, res) => {
+  try {
+    const CoinData = await Coin.findAll();
+    res.status(200).json(CoinData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get one Coin by id
+router.get('/:id', async (req, res) => {
+  try {
+    const CoinData = await Coin.findByPk(req.params.id);
+    if (!CoinData) {
+      res.status(404).json({ message: 'No action figure found with that id!' });
+      return;
+    }
+    res.status(200).json(CoinData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// create a new Coin
+router.post('/', async (req, res) => {
+  try {
+    const CoinData = await Coin.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(CoinData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// update a Coin by id
+router.put('/:id', async (req, res) => {
+  try {
+    const CoinData = await Coin.update(req.body, {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!CoinData) {
+      res.status(404).json({ message: 'No action figure found with that id!' });
+      return;
+    }
+    res.status(200).json(CoinData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete a Coin by id
+router.delete('/:id', async (req, res) => {
+  try {
+    const CoinData = await Coin.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!CoinData) {
+      res.status(404).json({ message: 'No action figure found with that id!' });
+      return;
+    }
+    res.status(200).json(CoinData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
