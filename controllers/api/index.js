dc9988d37046a8bbeb38ae8c990caa3144b1faf3
Drew Smith <drgh056@gmail.com>
2023-06-01 18:56:10 -0500

    updated api index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const actionFigureRoutes = require('./actionFigureRoutes')
+const actionFigureRoutes = require('./actionFigureRoutes');
+const coinRoutes = require('./coinRoutes');
+const musicRoutes = require('./musicRoutes');
+const cardRoutes = require('./cardRoutes')
 
 router.use('/users', userRoutes);
-router.use('/actionfigures', actionFigureRoutes)
+router.use('/actionfigure', actionFigureRoutes);
+router.use('/coin', coinRoutes);
+router.use('/music', musicRoutes);
+router.use('/card', cardRoutes);
 
 module.exports = router;
