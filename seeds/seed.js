5dfe93dded4ea7bd93f0816e6e89f8ebb6971562
Drew Smith <drgh056@gmail.com>
2023-05-31 21:55:18 -0500

    Updated seed files and models

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,11 @@
 const sequelize = require('../config/connection');
-const { User, ActionFigure } = require('../models');
+const { User, ActionFigure, Music, Coin, Card } = require('../models');
 
 const userData = require('./userData.json');
 const actionFigureData = require('./figuresData.json')
+const musicData = require('./musicData.json')
+const coinsData = require('./coinsData.json')
+const cardsData = require('./cardsData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -13,6 +16,21 @@ const seedDatabase = async () => {
   });
 
   const actionfigure = await ActionFigure.bulkCreate(actionFigureData, {
+    individualHooks: true, 
+    returning: true
+  })
+
+  const music = await Music.bulkCreate(musicData, {
+    individualHooks: true, 
+    returning: true
+  })
+
+  const coins = await Coin.bulkCreate(coinsData, {
+    individualHooks: true, 
+    returning: true
+  })
+
+  const cards = await Card.bulkCreate(cardsData, {
     individualHooks: true, 
     returning: true
   })
