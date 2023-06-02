f27406b768bf64b2e03ff465e40126316c217a75
ShimmyShong <lattabun@gmail.com>
2023-06-01 21:32:16 -0400

    semicolon

--- controllers/pages/index.js
+++ controllers/pages/index.js
@@ -10,13 +10,13 @@ const collections = require('./collection')
 
 router.use('/profile', withAuth, profile);
 
-router.use('/collection', withAuth, collections)
+router.use('/collections', withAuth, collections);
 
 router.use('/home', withAuth, home);
 
 router.use('/login', login);
 
-router.use('/sign-up', signUp)
+router.use('/sign-up', signUp);
 
 router.get('/', (req, res) => {
   return res.render('welcome');
