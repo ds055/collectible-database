928ac8c65d8ebcdee0026c143697d563c4312798
Drew Smith <drgh056@gmail.com>
2023-06-03 12:15:49 -0500

    Updated seeds and partials and routes

--- controllers/pages/dashboard/dashboardRoutes.js
+++ controllers/pages/dashboard/dashboardRoutes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
 
     res.render('dashboard', {
       ...userData,
-      logged_in: true
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,16 @@
 const sequelize = require('../config/connection');
-const { User, ActionFigure, Music, Coin, Card } = require('../models');
+const { User, ActionFigure, Music, Coin, Card, Collection, CollectionActionFigure, CollectionCard, CollectionCoin, CollectionMusic } = require('../models');
 
 const userData = require('./userData.json');
-const actionFigureData = require('./figuresData.json')
-const musicData = require('./musicData.json')
-const coinsData = require('./coinsData.json')
-const cardsData = require('./cardsData.json')
+const actionFigureData = require('./figuresData.json');
+const musicData = require('./musicData.json');
+const coinsData = require('./coinsData.json');
+const cardsData = require('./cardsData.json');
+const collectionData = require('./collectionData.json');
+const collectionActionFigure = require('./collectionActionFigureData.json');
+const collectionCoin = require('./collectionCoinData.json');
+const collectionCard = require('./collectionCardData.json');
+const collectionMusic = require('./collectionMusicData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -18,22 +23,47 @@ const seedDatabase = async () => {
   const actionfigure = await ActionFigure.bulkCreate(actionFigureData, {
     individualHooks: true, 
     returning: true
-  })
+  });
 
   const music = await Music.bulkCreate(musicData, {
     individualHooks: true, 
     returning: true
-  })
+  });
 
   const coins = await Coin.bulkCreate(coinsData, {
     individualHooks: true, 
     returning: true
-  })
+  });
 
   const cards = await Card.bulkCreate(cardsData, {
     individualHooks: true, 
     returning: true
-  })
+  });
+
+  const collection = await Collection.bulkCreate(collectionData, {
+    individualHooks: true, 
+    returning: true
+  });
+
+  const collectfigure = await CollectionActionFigure.bulkCreate(collectionActionFigure, {
+    individualHooks: true, 
+    returning: true
+  });
+
+  const collectcard = await CollectionCard.bulkCreate(collectionCard, {
+    individualHooks: true, 
+    returning: true
+  });
+
+  const collectcoin = await CollectionCoin.bulkCreate(collectionCoin, {
+    individualHooks: true, 
+    returning: true
+  });
+
+  const collectmusic = await CollectionMusic.bulkCreate(collectionMusic, {
+    individualHooks: true, 
+    returning: true
+  });
 
   process.exit(0);
 };
