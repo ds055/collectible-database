6eef43631b50694fc2df9e38c3af41920ace68c7
ShimmyShong <lattabun@gmail.com>
2023-06-06 00:56:31 -0400

    fix nav bar

--- controllers/pages/dashboard/collection/index.js
+++ controllers/pages/dashboard/collection/index.js
@@ -6,41 +6,41 @@ const router = require('express').Router();
 router.get('/', withAuth, async (req, res) => {
     try {
         const collectionData = await Collection.findAll({
-            where: {user_id: req.session.user_id}
+            where: { user_id: req.session.user_id }
         })
 
         const collection = collectionData.map((collection) => collection.get({ plain: true }))
 
         console.log(collection)
 
-        if(collection.length === 0){
+        if (collection.length === 0) {
             const noCollections = true;
-            res.render('bigEmpty', {noCollections})
-        }else {
+            res.render('bigEmpty', { noCollections, logged_in: req.session.logged_in })
+        } else {
             res.render('collections', { collection, logged_in: req.session.logged_in });
         }
 
     } catch (err) {
-    res.status(500).json(err);
+        res.status(500).json(err);
     }
 });
 
 router.get('/coin', withAuth, async (req, res) => {
-try {
-    // find all posts with User names via userId foreign key--returns only the username
-    const coinData = await Coin.findAll({
-        where: {user_id: 1}
-    });
+    try {
+        // find all posts with User names via userId foreign key--returns only the username
+        const coinData = await Coin.findAll({
+            where: { user_id: 1 }
+        });
 
-    // translate data to plain
-    const coin = coinData.map((coin) => coin.get({ plain: true}));
-    // render all-posts page with posts data and logged_in bool from session 
-    res.render('collections', { 
-        coin,
-        logged_in: req.session.logged_in
-    });
+        // translate data to plain
+        const coin = coinData.map((coin) => coin.get({ plain: true }));
+        // render all-posts page with posts data and logged_in bool from session 
+        res.render('collections', {
+            coin,
+            logged_in: req.session.logged_in
+        });
     } catch (err) {
-    res.status(500).json(err);
+        res.status(500).json(err);
     }
 })
 
@@ -48,65 +48,73 @@ router.get('/figure', withAuth, async (req, res) => {
     try {
         // find all posts with User names via userId foreign key--returns only the username
         const figureData = await ActionFigure.findAll({
-            where: {user_id: req.session.user_id}
+            where: { user_id: req.session.user_id }
         });
         // translate data to plain
-        const figure = figureData.map((figure) => figure.get({ plain: true}));
+        const figure = figureData.map((figure) => figure.get({ plain: true }));
         // render all-posts page with posts data and logged_in bool from session 
-        res.render('collections', { 
+        res.render('collections', {
             figure,
             logged_in: req.session.logged_in
         });
-        } catch (err) {
+    } catch (err) {
         res.status(500).json(err);
-        }
-    })
+    }
+})
 
-    // TODO: req.session.logged_in to other routes
+// TODO: req.session.logged_in to other routes
 router.get('/:id', withAuth, async (req, res) => {
     try {
         const collectionData = await Collection.findByPk(req.params.id, {
             include: [ActionFigure, Music, Coin, Card]
         })
-        
+
         const parsedData = collectionData.get({ plain: true })
 
         const collectionId = parsedData.id
 
-        if(parsedData.music.length > 0) {
+        if (parsedData.music.length > 0) {
             const music = parsedData.music;
-            res.render('collections', { music, collectionId, logged_in: req.session.logged_in  });
+            res.render('collections', { music, collectionId, logged_in: req.session.logged_in });
         }
-        else if(parsedData.action_figures.length > 0) {
+        else if (parsedData.action_figures.length > 0) {
             const figure = parsedData.action_figures;
             res.render('collections', { figure, collectionId, logged_in: req.session.logged_in });
         }
-        else if(parsedData.coins.length > 0) {
+        else if (parsedData.coins.length > 0) {
             const coin = parsedData.coins;
             res.render('collections', { coin, collectionId, logged_in: req.session.logged_in });
         }
-        else if(parsedData.cards.length > 0) {
+        else if (parsedData.cards.length > 0) {
             const card = parsedData.cards;
-            res.render('collections', { card, collectionId, logged_in: req.session.logged_in });    
+            res.render('collections', { card, collectionId, logged_in: req.session.logged_in });
         }
         else {
-            switch(parsedData.type) {
-                case 'Action Figure': 
-                    res.render('bigEmpty', { figure, collectionId, logged_in: req.session.logged_in})
+            switch (parsedData.collection_type) {
+                case 'Action Figure':
+                    const figure = parsedData.action_figures;
+                    res.render('bigEmpty', { figure, collectionId, logged_in: req.session.logged_in })
                     break;
                 case 'Coin':
-                    res.render('bigEmpty', { coin, collectionId, logged_in: req.session.logged_in})
+                    const coin = parsedData.coins;
+                    res.render('bigEmpty', { coin, collectionId, logged_in: req.session.logged_in })
                     break;
                 case 'Music':
-                    res.render('bigEmpty', { music, collectionId, logged_in: req.session.logged_in})
+                    const music = parsedData.music;
+                    res.render('bigEmpty', { music, collectionId, logged_in: req.session.logged_in })
                     break;
                 case 'Card':
-                    res.render('bigEmpty', { card, collectionId, logged_in: req.session.logged_in})
+                    const card = parsedData.cards;
+                    res.render('bigEmpty', { card, collectionId, logged_in: req.session.logged_in })
+                    break;
+                default:
+                    console.log("something? " + parsedData.collection_type)
+                    break;
             }
         }
-    
+
     } catch (err) {
-    res.status(500).json(err);
+        res.status(500).json(err);
     }
 });
 
