12b7f04320da2ce7f879656fd4175ed43469f8b8
Trevor McGill <trevor.mcgill@icloud.com>
2023-06-07 22:02:19 -0500

    linked js to edit collection items, updated README, deleted commented out code

--- models/getRecentItems.js
+++ models/getRecentItems.js
@@ -1,20 +1,5 @@
 const { Card, ActionFigure, Coin, Music } = require('../models');
-/* 
-async function getRecentItems() {
-  const recentCards = await Card.findAll({ order: [['createdAt', 'DESC']], limit: 12 });
-  const recentFigures = await ActionFigure.findAll({ order: [['createdAt', 'DESC']], limit: 12 });
-  const recentCoins = await Coin.findAll({ order: [['createdAt', 'DESC']], limit: 12 });
-  const recentMusic = await Music.findAll({ order: [['createdAt', 'DESC']], limit: 12 });
 
-  let recentItems = [...recentCards, ...recentFigures, ...recentCoins, ...recentMusic];
-  recentItems.sort((a, b) => b.createdAt - a.createdAt);  // Sorts items by descending createdAt timestamp
-  recentItems = recentItems.slice(0, 12);  // Gets the top 12 recent items across all collections
-
-  return recentItems.map(item => item.get({ plain: true }));
-}
-
-
- */
 async function getRecentItems() {
   const recentCards = await Card.findAll({ order: [['createdAt', 'DESC']], limit: 12 });
   const recentFigures = await ActionFigure.findAll({ order: [['createdAt', 'DESC']], limit: 12 });
